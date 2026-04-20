f9fe11eae799102c14657d835399defa24497f48
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-04-20 10:33:43 +0530

    chunk data fucntion defined

--- final_streamlit.py
+++ final_streamlit.py
@@ -37,3 +37,16 @@ def load_documnets(file):
     data=loader.load()
     return data
 
+
+# Chunk the Data 
+# Function that is used for Chunking the data 
+def chunk_data(data,chunk_size=256):
+    """
+    This function takes data loaded from lanchain_documentloaders and then chunks them 
+    data : Data loaded using lanchain documents loader
+    chunk_size : Provide an in integer ranging from 256 to any you like [256,512,1024 ]
+    """
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size)
+    chunks=text_splitter.split_documents(data)
+    return chunks
